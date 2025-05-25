c2dd2a5ebe53eae32f45361158061beb3a0b6d87
Oleg Babenko <majerkravchuk@gmail.com>
2025-05-25 21:09:14 +0200

    Add client & logs

---
+++ net-client/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Buffers.Binary;
+using System.IO.Compression;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private const int NEGOTIATION_HEADER   = 0x2A;
+    private const int USE_COMPRESSION_FLAG = 0x01;
+    // private const int USE_NO_COMPRESSION_FLAG = 0x00;
+
+    private static async Task Main(string[] args)
+    {
+        using TcpClient client = new("localhost", 4000);
+        Stream inputStream = client.GetStream();
+        Stream outputStream = inputStream;
+
+        WriteInt32BigEndian(outputStream, NEGOTIATION_HEADER);
+        WriteInt32BigEndian(outputStream, USE_COMPRESSION_FLAG);
+        // WriteInt32BigEndian(outputStream, USE_NO_COMPRESSION_FLAG);
+
+        if (ReadInt32BigEndian(inputStream) == NEGOTIATION_HEADER)
+        {
+            Console.WriteLine("successful negotiation");
+        }
+        else
+        {
+            Console.WriteLine("negotiation failed");
+            return;
+        }
+
+        if (ReadInt32BigEndian(inputStream) == USE_COMPRESSION_FLAG)
+        {
+            Console.WriteLine("switch to compression mode");
+            inputStream = new BrotliStream(inputStream, CompressionMode.Decompress);
+        }
+        else
+        {
+            Console.WriteLine("switch to compression mode failed, continue without compression");
+        }
+
+        while (true)
+        {
+            // Пинок сервера, "запрос"
+            WriteInt32BigEndian(outputStream, 0xF);
+
+            var length = ReadInt32BigEndian(inputStream);
+            var buffer = new byte[length];
+            inputStream.ReadExactly(buffer);
+
+            string response = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine($"Received: {response}");
+            await Task.Delay(1000);
+        }
+    }
+
+    private static void WriteInt32BigEndian(Stream stream, int value)
+    {
+        Span<byte> buffer = stackalloc byte[sizeof(int)];
+        BinaryPrimitives.WriteInt32BigEndian(buffer, value);
+        stream.Write(buffer);
+    }
+
+    private static int ReadInt32BigEndian(Stream stream)
+    {
+        Span<byte> buffer = new byte[sizeof(int)];
+        stream.ReadExactly(buffer);
+        return BinaryPrimitives.ReadInt32BigEndian(buffer);
+    }
+}
--- node-server/index.js
+++ node-server/index.js
@@ -1,7 +1,8 @@
 import { createServer } from 'node:net';
-import { brotliCompressSync, constants, createBrotliCompress } from 'node:zlib';
+import { PassThrough } from 'node:stream';
+import { constants, createBrotliCompress } from 'node:zlib';
 
-const SERVER_PORT = 7690;
+const SERVER_PORT = 4000;
 const NEGOTIATION_HEADER = 0x2A;
 const NEGOTIATION_HEADER_LENGTH = 4;
 const NEGOTIATION_PAYLOAD_LENGTH = 4;
@@ -9,12 +10,55 @@ const NEGOTIATION_LENGTH = NEGOTIATION_HEADER_LENGTH + NEGOTIATION_PAYLOAD_LENGT
 
 const USE_COMPRESSION_FLAG = 0x01;
 
+let counter = 0;
+
 const RESPONSE = "Earth is the third planet from the Sun and the only astronomical object known to harbor life. This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth's water is contained in its global ocean, covering 70.8% of Earth's crust. The remaining 29.2% of Earth's crust is land, most of which is located in the form of continental landmasses within Earth's land hemisphere. Most of Earth's land is at least somewhat humid and covered by vegetation, while large sheets of ice at Earth's polar deserts retain more water than Earth's groundwater, lakes, rivers, and atmospheric water combined. Earth's crust consists of slowly moving tectonic plates, which interact to produce mountain ranges, volcanoes, and earthquakes. Earth has a liquid outer core that generates a magnetosphere capable of deflecting most of the destructive solar winds and cosmic radiation."
+
+const patchSocket = (socket) => {
+    const compression = createBrotliCompress({
+        flush: constants.BROTLI_OPERATION_FLUSH,
+        finishFlush: constants.BROTLI_OPERATION_FINISH,
+    });
+
+    const before = new PassThrough({
+        transform: (chunk, encoding, callback) => {
+            console.log(`chunk size before: ${chunk.length}`);
+            callback(null, chunk);
+        }
+    });
+    const after = new PassThrough({
+        transform: (chunk, encoding, callback) => {
+            console.log(`chunk size after: ${chunk.length}`);
+            callback(null, chunk);
+        }
+    });
+
+    before.pipe(compression).pipe(after).pipe(socket);
+    socket.writeCompressed = (message) => {
+        before.write(message, 'binary', (err) => {
+            if (err) {
+                console.log('write error: ', err);
+                return;
+            }
+
+            compression.flush(constants.BROTLI_OPERATION_FLUSH, (flushErr) => {
+                if (err) {
+                    console.log('flush error: ', err);
+                    return;
+                }
+            });
+        });
+    }
+}
 
 const sendStringMessage = (socket, message) => {
     const header = Buffer.alloc(4);
     header.writeInt32BE(message.length);
-    socket.writeCompressed(Buffer.concat([header, Buffer.from(message)]));
+    if (socket.useCompression) {
+        socket.writeCompressed(Buffer.concat([header, Buffer.from(message)]));
+    } else {
+        socket.write(Buffer.concat([header, Buffer.from(message)]));
+    }
 };
 
 /**
@@ -28,6 +72,7 @@ const negotiateIfNotYet = (socket) => {
     if (negotiationHeader.readInt32BE() === NEGOTIATION_HEADER) {
         const response = Buffer.alloc(8, 0);
         response.writeInt32BE(NEGOTIATION_HEADER, 0);
+
         const negotiationPayload = socket.read(NEGOTIATION_PAYLOAD_LENGTH);
         if ((negotiationPayload.readInt32BE() & USE_COMPRESSION_FLAG) === USE_COMPRESSION_FLAG) {
             socket.useCompression = true;
@@ -36,28 +81,11 @@ const negotiateIfNotYet = (socket) => {
         } else {
             console.log('continue without compression');
         }
+
         socket.write(response);
         socket.isNegotiationDone = true;
-
-        const compression = createBrotliCompress({
-            flush: constants.BROTLI_OPERATION_FLUSH,
-            finishFlush: constants.BROTLI_OPERATION_FINISH,
-        });
-        compression.pipe(socket);
-        socket.writeCompressed = (message) => {
-            compression.write(message, 'binary', (err) => {
-                if (err) {
-                    console.log('write error: ', err);
-                    return;
-                }
-
-                compression.flush(constants.BROTLI_OPERATION_FLUSH, (flushErr) => {
-                    if (err) {
-                        console.log('flush error: ', err);
-                        return;
-                    }
-                });
-            });
+        if (socket.useCompression) {
+            patchSocket(socket);
         }
 
         return true;
@@ -78,7 +106,7 @@ export const onSocketReadeble = (socket) => {
     while ((message = socket.read(4)) !== null) {
         console.log('from client:', message);
         if (message.readInt32BE() === 0xF) {
-            sendStringMessage(socket, RESPONSE);
+            sendStringMessage(socket, `Server response number ${++counter} is that ${RESPONSE}`);
         }
     }
 };
